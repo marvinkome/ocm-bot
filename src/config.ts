cf02396d321534ec63ec4a8259f0ed9d9d6fa73a
Marvin Kome <marvinkome@gmail.com>
2020-12-30 12:58:30 +0100

    update championship sheetId

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export default {
     },
     sheets: {
         championship: {
-            sheetsId: "1o2kr3218nRU0R1HwiUg_cjwrfnmItCgZN7SS4VW9vzA",
+            sheetsId: "1m04BD0DE-BXbAHBUhufTvm4Gk63XowvBaEVV2JWqnOY",
             fixturesTab: "859971328",
             cardsTab: "1546579554",
             playerStatsTab: "1774202157",
